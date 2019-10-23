469fd44dbb94be9508cae8f2f5ee56cac104e9dd
Coxhuang <mhesat@outlook.com>
2019-10-23 22:29:20 +0800

    update

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -17,26 +17,26 @@
 
 <script>
 import LoginForm from '@/components/login-form'
-// import { mapActions } from 'vuex'
-export default {
-  components: {
-    LoginForm
-  },
-  // methods: {
-  //   ...mapActions([
-  //     'handleLogin',
-  //     'getUserInfo'
-  //   ]),
-  //   handleSubmit ({ userName, password }) {
-  //     this.handleLogin({ userName, password }).then(res => {
-  //       this.getUserInfo().then(res => {
-  //         this.$router.push({
-  //           name: this.$config.homeName
-  //         })
-  //       })
-  //     })
-  //   }
-  // }
-}
+	// import { mapActions } from 'vuex'
+	export default {
+		components: {
+			LoginForm
+	    },
+		// methods: {
+		// 	...mapActions([
+		// 		'handleLogin',
+		// 		'getUserInfo'
+		// 	]),
+		// 	handleSubmit ({ userName, password }) {
+		// 		this.handleLogin({ userName, password }).then(res => {
+		// 			this.getUserInfo().then(res => {
+		// 				this.$router.push({
+		// 					name: this.$config.homeName
+		// 				})
+		// 			})
+		// 		})
+		// 	}
+		// }
+	}
 </script>
 
