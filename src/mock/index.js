117e1c51ae181eaac5be1491ce54745fc82a1818
Coxhuang <mhesat@outlook.com>
2019-10-31 21:27:10 +0800

    update

---
+++ src/mock/index.js
@@ -0,0 +1,10 @@
+// import Mock from 'mockjs'
+//
+// const data={
+//     "id":"@guid",
+//     "name":"@cname",
+// };
+//
+// Mock.mock('/api/test/list', 'get', data);
+//
+// export default Mock;
