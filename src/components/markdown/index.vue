d098e7cc45fdc89691b1f7f943f49e9bcd733bfa
Coxhuang <mhesat@outlook.com>
2019-11-06 23:56:41 +0800

    update

--- src/components/markdown/index.vue
+++ src/components/markdown/index.vue
@@ -1,10 +1,24 @@
 <template>
 	<Row style="height: 100%;width: 100%;">
-		<Col span="24">
+		<Col span="24" style="height: 100%">
 			<Row>
-				<Col span="24">
+				<Col span="24" style="height:40px;margin-bottom: 24px">
+					<Row style="height: 100%">
+
+						<Col span="20" style="height: 100%;background-color: aquamarine">
+							<Input size="large" prefix="ios-paper-outline" placeholder="输入文章标题" style="height: 100%;width: 100%;" />
+						</Col>
+						<Col span="3" style="height: 100%" offset="1">
+							<Button type="error" size="large">发布文章</Button>
+						</Col>
+					</Row>
+				</Col>
+			</Row>
+			<Row style="height: 100%">
+				<Col span="24" style="height: 100%">
 					<mavon-editor
 						v-model="msg.mdValue"
+						style="height: 100%"
 					>
 					</mavon-editor>
 				</Col>
