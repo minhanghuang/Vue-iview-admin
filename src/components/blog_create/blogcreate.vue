10ab5d14d68958cae10dbff2ddd1a6291434ec8d
Coxhuang <mhesat@outlook.com>
2019-11-07 22:59:19 +0800

    update

--- src/components/blog_create/blogcreate.vue
+++ src/components/blog_create/blogcreate.vue
@@ -10,7 +10,11 @@
 					<Row style="height: 100%">
 
 						<Col span="20" style="height: 100%;background-color: aquamarine">
-							<Input v-model="title" type="text" clearable size="large" prefix="ios-paper-outline" placeholder="输入文章标题" style="height: 100%;width: 100%;" />
+							<Form ref="titleform" :model="form" :rules="rulestitle">
+								<FormItem prop="title">
+									<Input v-model="form.title" type="text" clearable size="large" prefix="ios-paper-outline" placeholder="输入文章标题" style="height: 100%;width: 100%;" />
+								</FormItem>
+							</Form>
 						</Col>
 						<Col span="3" style="height: 100%" offset="1">
 							<Button type="error" size="large" @click="create_blog_bt">发布文章</Button>
@@ -23,7 +27,6 @@
 					<child-markdown
 						ref="md"
 					>
-
 					</child-markdown>
 				</Col>
 			</Row>
@@ -41,13 +44,26 @@
         },
         data() {
             return {
-                title:"",
+                form:{
+                    title:""
+                },
+                rulestitle:{ // 校验表单规则
+                    title: [ // FormItem标签中的 prop 属性预期值
+                        { required: true, message: '标题不能为空', trigger: 'change' }
+                    ],
+                }
             }
         },
 	    methods:{
             create_blog_bt:function () { // 发布文章-按钮
 	            console.log(this.title);
 	            console.log(this.$refs.md.get_htlmvalue());
+                this.$refs.titleform.validate((valid) => {
+                    // this.$refs.loginForm.validate : 获取表单校验结果; 校验正确-> valid为True; 校验失败-> valid为False;
+                    if (valid) {
+
+                    }
+                })
             }
 	    }
     }
