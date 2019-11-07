c887f4b5afec5acb53fb1bbe646a5a1fc96a6e14
Coxhuang <mhesat@outlook.com>
2019-11-08 00:17:05 +0800

    update

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -10,46 +10,14 @@
 			height: inherit;
 		}
 	}
-	.layout-logo{
-		width: 100px;
-		height: 30px;
-		background: #5b6270;
-		border-radius: 3px;
-		float: left;
-		position: relative;
-		top: 15px;
-		left: 20px;
-	}
-	.layout-nav{
-		width: 420px;
-		margin: 0 auto;
-		margin-right: 20px;
-	}
+
 </style>
 <template>
 	<div class="layout">
 		<Layout>
 			<Header>
 				<Menu mode="horizontal" theme="dark" active-name="1">
-					<div class="layout-logo"></div>
-					<div class="layout-nav">
-						<MenuItem name="1">
-							<Icon type="ios-navigate"></Icon>
-							Item 1
-						</MenuItem>
-						<MenuItem name="2">
-							<Icon type="ios-keypad"></Icon>
-							Item 2
-						</MenuItem>
-						<MenuItem name="3">
-							<Icon type="ios-analytics"></Icon>
-							Item 3
-						</MenuItem>
-						<MenuItem name="4">
-							<Icon type="ios-paper"></Icon>
-							Item 4
-						</MenuItem>
-					</div>
+					<myhead></myhead>
 				</Menu>
 			</Header>
 			<Layout>
@@ -92,9 +60,9 @@
 						<BreadcrumbItem>Layout</BreadcrumbItem>
 					</Breadcrumb>
 					<Content :style="{padding: '24px', Height: 'auto', background: '#fff'}">
-						<my-main>
+						<mycontent>
 
-						</my-main>
+						</mycontent>
 					</Content>
 				</Layout>
 			</Layout>
@@ -102,10 +70,14 @@
 	</div>
 </template>
 <script>
-    import MyMain from '@/views/main/main'
+    import mycontent from '@/views/mycontent/mycontent'
+    import myhead from '@/views/myhead/myhead'
+
+
     export default {
         components:{
-            MyMain
+            mycontent,
+            myhead,
         },
         methods: {
             toroute(name) {
---
+++ src/views/myhead/myhead.vue
@@ -0,0 +1,69 @@
+<style lang="scss" scoped>
+	.layout-logo{
+		width: 100px;
+		height: 30px;
+		background: #5b6270;
+		border-radius: 3px;
+		float: left;
+		position: relative;
+		top: 15px;
+		left: 20px;
+	}
+	.layout-nav{
+		width: 420px;
+		margin: 0 auto;
+		margin-right: 20px;
+	}
+</style>
+
+<template>
+	<Row style="height: 100%;width: 100%">
+		<Col style="height: 100%;width: 100%">
+			<div class="layout-logo"></div>
+			<div class="layout-nav">
+				<Row>
+					<Col>
+						<Row>
+							<Col span="4" offset="20" style="">
+								<Dropdown style="margin-left: 20px" @on-click="headdown">
+									<Avatar style="background-color: rgba(93, 88, 177, 0.45)" icon="ios-person" />
+									<Icon type="ios-arrow-down"></Icon>
+									<DropdownMenu slot="list">
+										<DropdownItem name="center">个人中心</DropdownItem>
+										<DropdownItem name="about">关于我</DropdownItem>
+										<DropdownItem name="logout">退出</DropdownItem>
+									</DropdownMenu>
+								</Dropdown>
+							</Col>
+						</Row>
+					</Col>
+				</Row>
+			</div>
+		</Col>
+	</Row>
+</template>
+
+<script>
+    export default {
+        name: "myhead",
+        components: {},
+        data() {
+            return {}
+        },
+	    methods:{
+            headdown:function (name) {
+	            if (name == "logout"){
+                    localStorage.clear(); // 清空TOKEN
+                    this.$router.replace({ // 跳转到登录页面
+                        path: '/login',
+                    })
+	            }else if (name == "about"){
+
+	            } else {
+
+	            }
+            }
+	    }
+    }
+</script>
+
