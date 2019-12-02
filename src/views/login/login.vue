66d8d135966103badda099d4107e67da73c44915
Coxhuang <mhesat@outlook.com>
2019-12-02 22:01:18 +0800

    update
    更新登录页面 把背景图片删掉

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,11 +1,36 @@
 <style lang="less">
-  @import './login.less';
+	.login{
+		width: 100%;
+		height: 100%;
+		position: relative;
+		&-con{
+			position: absolute;
+			right: 45%;
+			top: 50%;
+			transform: translateY(-60%);
+			width: 300px;
+			&-header{
+				font-size: 16px;
+				font-weight: 300;
+				text-align: center;
+				padding: 30px 0;
+			}
+			.form-con{
+				padding: 10px 0 0;
+			}
+			.login-tip{
+				font-size: 10px;
+				text-align: center;
+				color: #c3c3c3;
+			}
+		}
+	}
 </style>
 
 <template>
 	<div class="login">
 		<div class="login-con">
-			<Card icon="log-in" title="欢迎登录" :bordered="false">
+			<Card icon="log-in" title="欢迎登录">
 				<div class="form-con">
 					<login-form
 						@on-success-valid="handleSubmit"
