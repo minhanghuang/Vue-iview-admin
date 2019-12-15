6e9adef48ff3ddee551dd91c3f10ff6186a01fe3
Coxhuang <mhesat@outlook.com>
2019-12-16 01:52:49 +0800

    update

--- src/views/mytimeline/mytimeline.vue
+++ src/views/mytimeline/mytimeline.vue
@@ -48,18 +48,10 @@
 					<div style="height: 500px" class="bottom-pane">
 						<Split v-model="splitvalue">
 							<div slot="left" class="bottom-split-pane">
-								<Collapse v-model="value1">
-									<Panel name="1">
-										史蒂夫·乔布斯
+								<Collapse v-model="bottom.left">
+									<Panel v-if="bottom.left.value!==undefined" :name="index" v-for="(item,index) in bottom.left.value" :key="index">
+										{{item.title}}
 										<p slot="content">史蒂夫·乔布斯（Steve Jobs），1955年2月24日生于美国加利福尼亚州旧金山，美国发明家、企业家、美国苹果公司联合创办人。</p>
-									</Panel>
-									<Panel name="2">
-										斯蒂夫·盖瑞·沃兹尼亚克
-										<p slot="content">斯蒂夫·盖瑞·沃兹尼亚克（Stephen Gary Wozniak），美国电脑工程师，曾与史蒂夫·乔布斯合伙创立苹果电脑（今之苹果公司）。斯蒂夫·盖瑞·沃兹尼亚克曾就读于美国科罗拉多大学，后转学入美国著名高等学府加州大学伯克利分校（UC Berkeley）并获得电机工程及计算机（EECS）本科学位（1987年）。</p>
-									</Panel>
-									<Panel name="3">
-										乔纳森·伊夫
-										<p slot="content">乔纳森·伊夫是一位工业设计师，现任Apple公司设计师兼资深副总裁，英国爵士。他曾参与设计了iPod，iMac，iPhone，iPad等众多苹果产品。除了乔布斯，他是对苹果那些著名的产品最有影响力的人。</p>
 									</Panel>
 								</Collapse>
 							</div>
@@ -69,14 +61,15 @@
 										<FormItem prop="pending">
 											<div class="my-form-items">
 												添加节点
-												<Button type="error" class="inner-item">+</Button>
+												<Button type="error" class="inner-item" @click="add_bt">+</Button>
+												{{bottom.right.count}}
 											</div>
 										</FormItem>
 										<FormItem prop="pending">
 											<Tooltip content="开启后,标记最后一个为幽灵节点" placement="bottom-start">
 												<div class="my-form-items">
 													幽灵模式
-													<i-switch v-model="bottom.right.pending" @on-change="change" class="inner-item"></i-switch>
+													<i-switch v-model="bottom.right.pending" class="inner-item"></i-switch>
 												</div>
 											</Tooltip>
 										</FormItem>
@@ -100,19 +93,33 @@
                 splitvalue: 0.7,
 	            bottom:{
                     right:{
-                        pending:true
+                        pending: true, // 幽灵模式
+                        count: 1, // 节点个数
+                    },
+                    left:{
+						value:[
+							{
+							    title:"节点1",
+                                content:"content",
+							}
+						]
                     }
 	            }
             }
         },
 	    watch:{
-            splitvalue:function (newval,oldval) {
+            splitvalue:function (newval, oldval) {
 	            if (newval > 0.7){
 	                this.splitvalue = 0.7; // 面板分割不能太右
 	            }
                 if (newval < 0.5){
                     this.splitvalue = 0.5 // 面板分割不能太左
                 }
+            }
+	    },
+	    methods:{
+            add_bt:function () { // 添加节点触发
+	            this.bottom.right.count ++; // 自增
             }
 	    }
     }
